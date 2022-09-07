87f71a88995e3b56c2fa1f31f2aca0237111ae52
Devin Berkani <devinberkani@gmail.com>
2022-09-07 08:15:25 -0400

    refine basic scoring logic

--- ReadabilityScorer/src/ReadabilityScorer.java
+++ ReadabilityScorer/src/ReadabilityScorer.java
@@ -8,8 +8,22 @@ public class ReadabilityScorer {
     }
 
     private String easyOrHard() {
-        int inputLength = scanner.nextLine().split("").length;
 
-        return inputLength <= 100 ? "EASY" : "HARD";
+        // splits sentences by a punctuation mark followed by a space
+        String[] sentences = scanner.nextLine().split("[.?!]\\s");
+
+        double sentenceCount = 0;
+        double wordCount = 0;
+        for (String sentence : sentences) {
+            sentenceCount++;
+            String[] currentSentence = sentence.split("\\s");
+            for (String currentWord : currentSentence) {
+                wordCount++;
+            }
+        }
+
+        double wordsInSentenceAverage = wordCount / sentenceCount;
+
+        return wordsInSentenceAverage <= 10 ? "EASY" : "HARD";
     }
 }
